6837029c987110ac9f35785433637378f7514aa7
나예진 <nyj500@konkuk.ac.kr>
2024-11-04 00:17:25 +0900

    fix: selection scene UI

--- Assets/Scripts/CharacterSelection.cs
+++ Assets/Scripts/CharacterSelection.cs
@@ -1,9 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CharacterSelection : MonoBehaviour
 {
+    public Button button1;
+    public Button button2;
+    public Button button3;
+    public Sprite originSprite;
+    public Sprite newSprite;
+
     void Start()
     {
         GameManager.instance.currentPlayer = GameManager.PlayerType.player1;
@@ -12,15 +19,24 @@ public class CharacterSelection : MonoBehaviour
     public void SetPlayerToPlayer1()
     {
         GameManager.instance.currentPlayer = GameManager.PlayerType.player1;
+        button1.image.sprite = newSprite;
+        button2.image.sprite = originSprite;
+        button3.image.sprite = originSprite;
     }
 
     public void SetPlayerToPlayer2()
     {
         GameManager.instance.currentPlayer = GameManager.PlayerType.player2;
+        button1.image.sprite = originSprite;
+        button2.image.sprite = newSprite;
+        button3.image.sprite = originSprite;
     }
 
     public void SetPlayerToPlayer3()
     {
         GameManager.instance.currentPlayer = GameManager.PlayerType.player3;
+        button1.image.sprite = originSprite;
+        button2.image.sprite = originSprite;
+        button3.image.sprite = newSprite;
     }
 }
--- Assets/Scripts/LoadSelectionScene.cs
+++ Assets/Scripts/LoadSelectionScene.cs
@@ -2,12 +2,17 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class LoadSelectionScene : MonoBehaviour
 {
+    public Button startButton;
+    public Sprite clickedImage;
+
     public void OnStartButtonClicked()
     {
         SceneManager.LoadScene("SelectionScene");
+        startButton.image.sprite = clickedImage;
     }
 
 }
